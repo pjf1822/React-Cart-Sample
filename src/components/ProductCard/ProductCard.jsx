a2fd1b0e783b6c938d49cb1a6d73f431dd873f36
Peter Forbes <66070260+pjf1822@users.noreply.github.com>
2023-03-03 16:35:04 -0500

    Cleaned up the Product Cards

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -10,25 +10,26 @@ const ProductCard = ({ product }) => {
     <div className="productCard__wrapper">
       <div>
         <img className="productCard__img" src={product.image} alt="" />
-        <h4>{product.name}</h4>
-        <div className="ProductCard__price">
-          <h5>
-            <NumericFormat
-              allowLeadingZeros
-              value={product.price}
-              displayType={"text"}
-              thousandSeparator={true}
-              prefix={"$"}
-            />
-          </h5>
+        <div className="productCard_contentWrapper">
+          <h4 className="ProductCard__name">{product.name}</h4>
+          <div className="ProductCard__price">
+            <h5>
+              <NumericFormat
+                allowLeadingZeros
+                value={product.price}
+                displayType={"text"}
+                thousandSeparator={true}
+                prefix={"$"}
+              />
+            </h5>
+          </div>
+          <button
+            className="ProductCard__button"
+            onClick={() => addToCart(product)}
+          >
+            Add to basket
+          </button>
         </div>
-        <div className="ProductCard__Rateing"></div>
-        <button
-          className="ProductCard__button"
-          onClick={() => addToCart(product)}
-        >
-          Add to basket
-        </button>
       </div>
     </div>
   );
--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import ProductCard from "../../components/ProductCard/ProductCard.jsx";
 import products from "../../data.js";
+import "./Homepage.css";
 
 import { CartContext } from "../../context/CartState.js";
 
@@ -8,10 +9,12 @@ const Homepage = () => {
   const { navHeight } = useContext(CartContext);
 
   return (
-    <div style={{ marginTop: navHeight }}>
-      {products.map((product, i) => {
-        return <ProductCard product={product} key={product._id} />;
-      })}
+    <div style={{ marginTop: navHeight + 10 }}>
+      <div className="productsWrapper">
+        {products.map((product, i) => {
+          return <ProductCard product={product} key={product._id} />;
+        })}
+      </div>
     </div>
   );
 };
